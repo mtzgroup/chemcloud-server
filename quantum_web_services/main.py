e3e18beb50ef51df4dc6db5b08fa6163dba18516
Colton Hicks <github@coltonhicks.com>
2020-11-09 18:15:12 -0800

    Integrated qcelemental and qcengine for basic computations.

--- quantum_web_services/main.py
+++ quantum_web_services/main.py
@@ -1,4 +1,6 @@
+import qcengine as qcng
 from fastapi import FastAPI
+from qcelemental.models.results import AtomicInput, AtomicResult
 
 app = FastAPI()
 
@@ -6,3 +8,9 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     return {"Hello": "World"}
+
+
+@app.post("/compute")
+def compute(atomic_input: AtomicInput) -> AtomicResult:
+    result = qcng.compute(atomic_input, "psi4")
+    return result
