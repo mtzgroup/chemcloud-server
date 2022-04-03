c5f22384b98659dcb15b97de0ecdadff0b2e7dae
Colton Hicks <github@coltonhicks.com>
2022-04-02 18:31:43 -0700

    Simplify result getting down to single id. Save task strucuture to backend using task_id as key

--- terachem_cloud/routes/compute.py
+++ terachem_cloud/routes/compute.py
@@ -1,19 +1,20 @@
-from typing import Optional, Union
+from typing import Optional, Type, Union
 
-from fastapi import APIRouter
+from celery.result import AsyncResult
+from fastapi import APIRouter, BackgroundTasks, HTTPException, Path
 
 from terachem_cloud.config import get_settings
 from terachem_cloud.models import (
     AtomicInputOrList,
-    FutureResult,
-    FutureResultGroup,
     OptimizationInputOrList,
+    Result,
+    ResultGroup,
     SupportedEngines,
     SupportedProcedures,
+    TaskState,
 )
-from terachem_cloud.task_models import GroupTask, Task
 
-from .helpers import _bytes_to_b64, compute_inputs_async
+from .helpers import _bytes_to_b64, compute_inputs_async, delete_result, restore_result
 
 settings = get_settings()
 
@@ -23,64 +24,63 @@ router = APIRouter()
 @router.post(
     "",  # NOTE: "/compute" prefix is prepended in top level main.py file
     # Appears correct: https://fastapi.tiangolo.com/tutorial/extra-models/?h=union#union-or-anyof
-    response_model=Union[GroupTask, Task],  # type: ignore
-    response_description="Task ID(s) for the requested computation.",
+    response_description="Task ID for the requested computation.",
 )
 async def compute(
     input_data: AtomicInputOrList,
     engine: SupportedEngines,
     queue: Optional[str] = None,
-) -> Union[GroupTask, Task]:
+) -> str:
     """Submit a computation: AtomicInput (or list) and computation engine."""
     return compute_inputs_async(input_data, engine, queue)
 
 
 @router.post(
     "-procedure",  # NOTE: "/compute" prefix is prepended in top level main.py file
-    # Appears correct: https://fastapi.tiangolo.com/tutorial/extra-models/?h=union#union-or-anyof
-    response_model=Union[GroupTask, Task],  # type: ignore
-    response_description="Task ID(s) for the requested computation.",
+    response_description="Task ID for the requested computation.",
 )
 async def compute_procedure(
     input_data: OptimizationInputOrList,
     procedure: SupportedProcedures,
     queue: Optional[str] = None,
-) -> Union[GroupTask, Task]:
+) -> str:
     """Submit a computation: OptimizationInput (or list) and procedure"""
     return compute_inputs_async(input_data, procedure, queue)
 
 
-@router.post(
-    "/result",  # NOTE: "/compute" prefix is prepended in top level main.py file
-    # Appears correct: https://fastapi.tiangolo.com/tutorial/extra-models/?h=union#union-or-anyof
-    response_model=Union[FutureResultGroup, FutureResult],  # type: ignore
+@router.get(
+    "/result/{result_id}",  # NOTE: "/compute" prefix is prepended in top level main.py file
+    response_model=Union[ResultGroup, Result],  # type: ignore
     response_description="A compute task's status and (if complete) return value.",
 )
 async def result(
-    task: Union[GroupTask, Task],
-    # background_tasks: BackgroundTasks,
-) -> Union[FutureResultGroup, FutureResult]:
-    """Retrieve a compute task's status and result (if ready)."""
-    # import pdb; pdb.set_trace()
-    result = task.to_result()
+    background_tasks: BackgroundTasks,
+    result_id: str = Path(
+        ...,
+        title="The ID of the result to get",
+        regex=r"[0-9a-f]{8}\-[0-9a-f]{4}\-4[0-9a-f]{3}\-[89ab][0-9a-f]{3}\-[0-9a-f]{12}",
+    ),
+) -> Union[ResultGroup, Result]:
+    """Retrieve a result's status and (if complete) output."""
+    try:
+        result = restore_result(result_id)
+    except ValueError:
+        raise HTTPException(
+            status_code=410, detail="Result has already been deleted from server"
+        )
 
-    # Transform any binary native_files to b64 encoded string
-    if result.result:
-        _bytes_to_b64(result.result)
+    state = TaskState.PENDING
+    data = None
 
-        # if result.compute_status in states.READY_STATES:
-        # Remove result from backend
-        # NOTE: Need to update to use background_task so only executes if task returned
-        # successfully.
-        # Will only execute if result returned successfully
-        # background_tasks.add_task(delete_task, task)
-        # TODO: Use background_tasks; currently having issues with AsyncResult.get()
-        # being called twice. the .get() call in GroupTask.to_result() hangs forever
-        # for reasons I don't fully understand
-        task.forget()
-    return result
+    if result.ready():
+        state = TaskState.COMPLETE
+        data = result.get()
+        # Transform any binary native_files and .extras to b64 encoded string
+        _bytes_to_b64(data)
+        # Remove result from backend AFTER function returns successfully
+        background_tasks.add_task(delete_result, result)
 
-
-def delete_task(task: Union[Task, GroupTask]) -> None:
-    """Delete Celery result(s) from backend"""
-    task.forget()
+    fr_model: Union[Type[Result], Type[ResultGroup]] = (
+        Result if isinstance(result, AsyncResult) else ResultGroup
+    )
+    return fr_model(state=state, result=data)
